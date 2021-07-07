d4d698305acb937293b45f6e2d481e71fd7262af
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-07 11:03:25 +0900

    [add] 属性を利用する

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+
+  render(){
+    return (
+      <div>
+        <h1 className="bg-primary text-white display-4">React</h1>
+        <div className="container">
+          <p className="subtitle">{this.title}</p>
+          <p>{this.message}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+/*function App() {
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
@@ -11,6 +33,6 @@ function App() {
       </div>
     </div>
   );
-}
+}*/
 
 export default App;

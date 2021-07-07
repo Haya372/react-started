b882ad0ff76b01b0b930f75294f571dc80c61cbf
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-07 16:32:23 +0900

    イベントをバインドしよう

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      msg: 'Hello',
-      count: 0
+      msg: 'count start!',
+      counter: 0
     };
-    let timer = setInterval(() => {
-      this.setState({
-        count: this.state.count + 1,
-        msg: "[ count: " + this.state.count + " ]"
-      });
-    }, 1000);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(event){
+    this.setState({
+      counter: this.state.counter + 1,
+      msg: '*** count: ' + this.state.counter + ' ***'
+    })
   }
 
   render(){
@@ -23,9 +25,11 @@ class App extends Component {
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <p className="subtitle">Show message.</p>
-          <p className="alert alert-warning">{this.state.msg}</p>
-          <p className="alert alert-dark">{this.props.msg}</p>
+          <p className="subtitle">Count number.</p>
+          <div className="alert alert-primary text-center">
+            <p className="h5 mb-4">{this.state.msg}</p>
+            <button className="btn btn-primary" onClick={this.doAction}>Click</button>
+          </div>
         </div>
         <div className="container p-relative">
           <p className="subtitle">draw rectangle.</p>

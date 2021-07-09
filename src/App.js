5213606e5db2562d1c2286b4f172b09c0388b63e
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-09 16:57:21 +0900

    フックでステートを表示する

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message] = useState("Welcome to Hooks");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <h4 className="my-3">Hooks sample</h4>
+        <div className="alert alert-primary text-center">
+          <p className="h5">{message}.</p>
+        </div>
+      </div>
     </div>
   );
 }

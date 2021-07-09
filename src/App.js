c843aceeb1f9fc8b42075a5c77f5dee839b0729d
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-09 21:33:45 +0900

    複数の副作用フック

--- src/App.js
+++ src/App.js
@@ -10,20 +10,32 @@ function AlertMessage(props){
 }
 
 function App() {
-  const [val, setVal] = useState(0);
-  const [msg, setMsg] = useState('set a number...');
+  const [val, setVal] = useState(10000);
+  const [tax1, setTax1] = useState(0);
+  const [tax2, setTax2] = useState(0);
+  const [msg, setMsg] = useState('set a price...');
 
   const doChange = (event) => {
     setVal(event.target.value);
   }
 
+  const doAction = () => {
+    let res = (
+      <div>
+        <p>軽減税率（８％）：{tax1}円</p>
+        <p>軽減税率（１０％）：{tax2}円</p>
+      </div>
+    )
+    setMsg(res);
+  }
+
   useEffect(() => {
-    let total = 0;
-    for(let i = 0; i <= val; i++){
-      total += i;
-    }
-    setMsg("total: " + total + ".");
-  })
+    setTax1(Math.floor(val * 1.08));
+  });
+
+  useEffect(() => {
+    setTax2(Math.floor(val * 1.1));
+  });
 
   return (
     <div>
@@ -35,6 +47,7 @@ function App() {
           <label>Input:</label>
           <input type="number" className="form-control" onChange={doChange} />
         </div>
+        <button className="btn btn-primary" onClick={doAction}>Calc</button>
       </div>
     </div>
   );

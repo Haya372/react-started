96a672d2ad18136ba51a6524f4fe80cafeec4936
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-09 13:28:28 +0900

    コンテキストでテーマを作る

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
 import './App.css';
 import React, { Component } from 'react';
 
-let data = {
-  title: 'React-Context',
-  message: 'this is sample message'
+let theme = {
+  light: {
+    styles: {
+      backgroundColor: "#f0f9ff",
+      color: "#00f"
+    },
+    head: "bg-primary text-white display-4 mb-4",
+    alert: "alert alert-primary my-3",
+    text: "text-primary m-3",
+    foot: "py-4"
+  },
+  dark: {
+    styles: {
+      backgroundColor: "#336",
+      color: "#eef"
+    },
+    head: "bg-secondary text-white display-4 mb-4",
+    alert: "alert alert-dark my-3",
+    text: "text-light m-3",
+    foot: "py-4"
+  },
 };
 
-const SampleContext = React.createContext(data);
+const ThemeContext = React.createContext(theme.light);
 
 class App extends Component {
-  
-  newdata = {
-    title: '新しいタイトル',
-    message: 'これは新しいメッセージです。'
-  }
-
-  constructor(props){
-    super(props);
-    this.state = {
-      title: 'input form',
-      message: 'type your name.',
-      max: 10
-    }
-    this.doCheck = this.doCheck.bind(this);
-  }
-
-  doCheck(e){
-    alert(e.target.value + "は長すぎます。（最大" + this.state.max + "文字）") 
-  }
-
+  static contextType = ThemeContext;
   render(){
     return (
-      <div>
-        <h1 className="bg-primary text-white display-4">React</h1>
+      <div style={this.context.styles}>
+        <h1 className={this.context.head}>React</h1>
         <div className="container">
-          <Title />
-          <Message />
-          <hr />
-          <SampleContext.Provider value={this.newdata}>
-            <Title />
-            <Message />
-          </SampleContext.Provider>
-          <hr />
-          <Title />
-          <Message />
+          <Title value="Content page" />
+          <Message value="This is Content sample."/>
+          <Message value="※これはテーマのサンプルです。"/>
+          <div className={this.context.foot}></div>
         </div>
       </div>
     )
@@ -51,24 +44,24 @@ class App extends Component {
 }
 
 class Title extends Component {
-  static contextType = SampleContext;
+  static contextType = ThemeContext;
 
   render(){
     return (
-      <div className="card p-2 my-3">
-        <h2>{this.context.title}</h2>
+      <div className={this.context.alert}>
+        <h2 style={this.context.style}>{this.props.value}</h2>
       </div>
     )
   }
 }
 
 class Message extends Component {
-  static contextType = SampleContext;
+  static contextType = ThemeContext;
 
   render(){
     return (
-      <div className="alert alert-primary">
-        <p>{this.context.message}</p>
+      <div style={this.context.style}>
+        <p className={this.context.text}>{this.props.value}</p>
       </div>
     )
   }

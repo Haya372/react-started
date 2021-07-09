48f4f8c231e4ec036b0363b4e582d381fd125ca4
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-09 17:17:27 +0900

    ステートは複数作れる

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
   const onClickFunc = () => {
     setCount(count + 1);
+  }
+  const changeFlag = (e) => {
+    setFlag(e.target.checked);
   }
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
+        {flag?
         <div className="alert alert-primary text-center">
           <p className="h5 mb-3">click: {count} times!</p>
           <div><button className="btn btn-primary" onClick={onClickFunc}>Click me</button></div>
+        </div>
+        :
+        <div className="card p-3 border-primary text-center">
+          <p className="h5 mb-3 text-left text-primary">click: {count} times!</p>
+          <div><button className="btn btn-primary" onClick={onClickFunc}>Click me</button></div>
+        </div>
+        }
+        <div className="form-group h6 text-center pt-3">
+          <input type="checkbox" className="form-check-input" id="check1" onChange={changeFlag}/>
+          <label className="form-check-label" htmlFor="check1">
+            Change form style.
+          </label>
         </div>
       </div>
     </div>

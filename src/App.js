26c5d206a096220a49520f3725c28fa3c39cdbc3
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-09 12:33:36 +0900

    フォームの利用

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import './App.css';
 import React, { Component } from 'react';
 import Rect from './Rect';
-import Message from './Message';
 
 class App extends Component {
   
   input = ''
+
+  constructor(props){
+    super(props);
+    this.state = {
+      title: 'input form',
+      message: 'type your name.'
+    }
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+  }
+
+  doChange(e){
+    this.input = e.target.value;
+  }
+
+  doSubmit(e){
+    this.setState({
+      title: 'send form',
+      message: 'Hello, ' + this.input + '!!'
+    });
+    e.preventDefault();
+  }
 
   render(){
     return (
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <Message title="Children!">
-            これはコンポーネント内のコンテンツです。
-            マルでテキストを分割し、リストにして表示します。
-            改行は必要ありません。
-          </Message>
+          <h4>{this.state.title}</h4>
+          <p className="card h5 p-3">{this.state.message}</p>
+          <div className="alert alert-primary mt-3">
+            <form onSubmit={this.doSubmit}>
+              <div className="form-group">
+                <label>Message:</label>
+                <input type="text" className="form-control" onChange={this.doChange} />
+              </div>
+              <input type="submit" className="btn btn-primary" value="Click" />
+            </form>
+          </div>
         </div>
         <div className="container p-relative">
           <p className="subtitle">draw rectangle.</p>

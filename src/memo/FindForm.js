68ca918f5d2d9dff6da0ed7172f3df1c3a7d694e
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-10 09:26:51 +0900

    FindFormコンポーネントを作る

---
+++ src/memo/FindForm.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import usePersist from '../Persist';
+
+function FindForm(props){
+  const [memo, setMemo] = usePersist("memo", []);
+  const [fmemo, setFmemo] = usePersist("findMemo", []);
+  const [message, setMessage] = useState('');
+  const [mode, setMode] = usePersist('mode', 'find');
+
+  const doChange = (e) => {
+    setMessage(e.target.value);
+  }
+
+  const doAction = (e) => {
+    if(message == ''){
+      setMode('default');
+      return;
+    }
+    let res = memo.filter((item, key) => {
+      return item.message.includes(message);
+    });
+    setFmemo(res);
+    setMode('find');
+    setMessage('');
+  }
+
+  return (
+    <form onSubmit={doAction} action="">
+      <div className="form-group row">
+        <input type="text" value={message} onChange={doChange} className="form-control-sm col" />
+        <input type="submit" value="Find" className="btn btn-primary btn-sm col-2" />
+      </div>
+    </form>
+  )
+}
+
+export default FindForm;
